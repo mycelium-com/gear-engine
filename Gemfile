42cf7daf1d54d0edba64d26a3669c5d8b3a49e4b
AlexanderPavlenko <AlexanderPavlenko@users.noreply.github.com>
2022-11-30 18:19:08 +0400

    configures order status check intervals

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.7'
 
 gem 'dotenv-rails', require: 'dotenv/rails-now'
-gem 'envied'
+gem 'envied', '~> 0.9.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
 # Use postgresql as the database for Active Record
--- spec/interactors/order_schedule_status_check_spec.rb
+++ spec/interactors/order_schedule_status_check_spec.rb
@@ -15,10 +15,49 @@ RSpec.describe OrderScheduleStatusCheck, type: :interactor do
         expect(time).to be_instance_of Time
         expect(OrderStatusCheckJob).to have_been_enqueued.at(time).with(order: order, final: final)
       end
-      result.schedule.each_cons(2) do |(a, b)|
+      prev_interval = 0
+      intervals = result.schedule.each_cons(2)
+      intervals.each_with_index do |(a, b), i|
         expect(a).to be < b
+        if i + 1 != intervals.size # last check ignores backoff
+          expect(b - a).to be >= prev_interval
+        end
+        prev_interval = b - a
       end
       expect(result.schedule.last).to eq(order.created_at + order.gateway.orders_expiration_period)
+    end
+
+    it "is configurable via ENV vars" do
+      ENV['ORDER_CHECK_BASE_INTERVAL'] = '1'
+      ENV['ORDER_CHECK_MAX_INTERVAL']  = '1'
+      ENV['ORDER_CHECK_BACKOFF_MULT']  = '1.0'
+      ENV['ORDER_CHECK_RAND_FACTOR']   = '0.0'
+      result = described_class.call(order: order)
+      expect(result.schedule.size).to eq(300)
+
+      ENV['ORDER_CHECK_BASE_INTERVAL'] = '3'
+      ENV['ORDER_CHECK_MAX_INTERVAL']  = '3'
+      ENV['ORDER_CHECK_BACKOFF_MULT']  = '1.0'
+      ENV['ORDER_CHECK_RAND_FACTOR']   = '0.0'
+      result = described_class.call(order: order)
+      expect(result.schedule.size).to eq(100)
+
+      # the new defaults
+      ENV['ORDER_CHECK_BASE_INTERVAL'] = '3'
+      ENV['ORDER_CHECK_MAX_INTERVAL']  = '600'
+      ENV['ORDER_CHECK_BACKOFF_MULT']  = '1.0027'
+      ENV['ORDER_CHECK_RAND_FACTOR']   = '0.0'
+
+      order.gateway.orders_expiration_period = 900
+      order.gateway.save_changed
+      result = described_class.call(order: order)
+      expect(result.schedule.size).to eq(221)
+
+      order.gateway.orders_expiration_period = 48 * 3600
+      order.gateway.save_changed
+      result = described_class.call(order: order)
+      expect(result.schedule.size).to eq(1875)
+      # puts result.schedule.inspect
     end
   end
 end
